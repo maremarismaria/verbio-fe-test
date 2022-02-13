4a1a500cff919933554dca51e1016c03819ee4af
Maria Morales <maremarismaria@gmail.com>
2022-02-13 16:54:04 +0000

    :construction: Add BE communication to the Chat page

--- src/components/pages/Chat/Chat.tsx
+++ src/components/pages/Chat/Chat.tsx
@@ -1,22 +1,20 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Input from "../../atoms/Input/Input"
-import NullTheCat from "../../../assets/images/NullTheCat.png"
 import { authContext } from "../../../contexts/AuthContext"
 import { getHour } from "../../../utils/Date"
+import { BotMessage } from "../../../API/API"
+import NullTheCat from "../../../assets/images/NullTheCat.png"
+import MochaTheChihuahua from "../../../assets/images/MochaTheChihuahua.jpg"
 import "./Chat.scss"
 
 /**
  * Notes about the chat component:
  *
  * I would nice to implement an emoji picker, but time runs out!
+ * Nice to have: initialize messages list using Local Storage
  */
 
-export interface SendMessageResponse {
-    text?: string
-    url?: string
-}
-
-interface ChatMessage extends SendMessageResponse {
+interface ChatMessage extends BotMessage {
     sender: {
         name: string
         avatar: string
@@ -29,6 +27,58 @@ const Chat: React.FC = () => {
     const formRef: React.RefObject<HTMLFormElement> = React.createRef()
     const [messages, setMessages] = useState<ChatMessage[]>([])
 
+    useEffect(() => {
+        getWelcomeMessage()
+    }, [])
+
+    const updateMessages = (message: ChatMessage) => {
+        setMessages((currentMessages) => [...currentMessages, message])
+    }
+
+    const parseMessage = (sender: string, avatar: string, text: string) => {
+        return {
+            sender: {
+                name: sender,
+                avatar,
+            },
+            timestamp: new Date(),
+            text,
+            // TODO Image type
+            type: "",
+        }
+    }
+
+    const setServerMessages = (messages: BotMessage[] | undefined) => {
+        if (messages && !!messages.length) {
+            for (let i = 0; i < messages.length; i++) {
+                const text = messages[i].text || ""
+                const message = parseMessage("Server", MochaTheChihuahua, text)
+                updateMessages(message)
+            }
+        }
+    }
+
+    const getWelcomeMessage = async () => {
+        try {
+            const messages = await auth.getWelcomeMessage()
+            setServerMessages(messages)
+        } catch (e) {
+            console.error(e.toString())
+        }
+    }
+
+    const sendMessage = async (text: string) => {
+        try {
+            const messages = await auth.sendMessage(text)
+            const userMessage = parseMessage(auth.user, NullTheCat, text)
+            updateMessages(userMessage)
+
+            // TODO Image type
+        } catch (e) {
+            console.error(e.toString())
+        }
+    }
+
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
@@ -38,16 +88,7 @@ const Chat: React.FC = () => {
             return
         }
 
-        const message = {
-            sender: {
-                name: auth.user,
-                avatar: NullTheCat,
-            },
-            timestamp: new Date(),
-            text,
-        } as ChatMessage
-
-        setMessages([...messages, message])
+        sendMessage(text)
 
         if (formRef.current) {
             formRef.current.reset()
@@ -65,7 +106,7 @@ const Chat: React.FC = () => {
                     messages.map((message) => (
                         <li className={"message-item"}>
                             <img
-                                className={"sender"}
+                                className={"avatar"}
                                 width={"32px"}
                                 height={"32px"}
                                 alt={message.sender.name}
@@ -87,8 +128,8 @@ const Chat: React.FC = () => {
             <h2>Chat</h2>
 
             <div className={"chat-header"}>
-                <p>Server</p>
-                <p>last seen recently</p>
+                <p className={"receiver"}>Server</p>
+                <p className={"status"}>Online</p>
             </div>
 
             {renderMessages()}

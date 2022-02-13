e3249a4b7f963a1e2240360e00585e4c48d5f76d
Maria Morales <maremarismaria@gmail.com>
2022-02-13 16:51:50 +0000

    :construction: Add requests with auth to the AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import useToken from "../hooks/useToken"
+import * as API from "../API/API"
 
 interface AuthContextType {
     user: any
-    signin: (user: string, callback: VoidFunction) => void
-    signout: (callback: VoidFunction) => void
+    signin: (credentials: API.Credentials, callback: Function) => void
+    signout: (callback: Function) => void
+    getWelcomeMessage: () => Promise<API.BotMessage[] | undefined>
+    sendMessage: (text: string) => Promise<API.BotMessage[] | undefined>
 }
 
 const AuthContext = React.createContext<AuthContextType>(null!)
@@ -13,43 +16,48 @@ export function authContext() {
     return React.useContext(AuthContext)
 }
 
-const fakeAuthProvider = {
-    isAuthenticated: false,
-    signin(callback: VoidFunction) {
-        fakeAuthProvider.isAuthenticated = true
-        setTimeout(callback, 100) // fake async
-    },
-    signout(callback: VoidFunction) {
-        fakeAuthProvider.isAuthenticated = false
-        setTimeout(callback, 100)
-    },
-}
-
 export function AuthContextProvider({
     children,
 }: {
     children: React.ReactNode
 }) {
     const { token, setToken } = useToken()
-    const [user, setUser] = React.useState<string | null>(token)
+    const [user, setUser] = React.useState<string | null>()
 
-    const signin = (newUser: string, callback: VoidFunction) => {
-        return fakeAuthProvider.signin(() => {
-            setUser(newUser)
-            setToken("le-token")
+    const signin = async (credentials: API.Credentials, callback: Function) => {
+        try {
+            const response = await API.login(credentials)
+            setUser(credentials.user)
+            setToken(response.session_id)
             callback()
-        })
+        } catch (e) {
+            console.error(e.toString())
+        }
     }
 
-    const signout = (callback: VoidFunction) => {
-        return fakeAuthProvider.signout(() => {
-            setUser(null)
-            setToken(null)
-            callback()
-        })
+    const signout = async (callback: Function) => {
+        setUser(null)
+        setToken(null)
+        callback()
     }
 
-    const value = { user, signin, signout }
+    const getWelcomeMessage = async () => {
+        try {
+            return await API.getWelcomeMessage(token || "")
+        } catch (e) {
+            console.error(e.toString())
+        }
+    }
+
+    const sendMessage = async (text: string) => {
+        try {
+            return await API.sendMessage(token || "", text)
+        } catch (e) {
+            console.error(e.toString())
+        }
+    }
+
+    const value = { user, signin, signout, getWelcomeMessage, sendMessage }
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }

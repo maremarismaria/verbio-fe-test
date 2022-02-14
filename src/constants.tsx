5ff1d10536206c33e15e894677b5012e0a37c166
Maria Morales <maremarismaria@gmail.com>
2022-02-14 14:01:33 +0000

    :truck: Add .env sample file and remove the fallback endpoint from the constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5556"
+export const API_URL = process.env.REACT_APP_API_URL
 
 export const STORAGE_KEY = "verbio-app-auth"
 

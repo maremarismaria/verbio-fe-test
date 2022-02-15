c9100bec114891ab9458ed37cb225c2bb5e400ae
Maria Morales <maremarismaria@gmail.com>
2022-02-15 08:31:11 +0000

    :white_check_mark: Add Login test suite

--- cypress/tests/login.test.js
+++ cypress/tests/login.test.js
@@ -1,11 +1,25 @@
-describe("Login", () => {
+describe("Login suite", () => {
     beforeEach(() => {
-        cy.viewport("macbook-15")
+        cy.viewport("iphone-xr")
+        cy.flushSession()
     })
 
     it("Should login", () => {
         cy.login({ user: "admin", password: "admin" })
         cy.url().should("include", "/chat")
         cy.get("article.Chat")
+    })
+
+    it("Should not login due to bad credentials", () => {
+        cy.login({ user: "admin", password: "wrong" })
+        cy.get("article.Login")
+    })
+
+    it("Should not send the login form because some field is empty or invalid", () => {
+        cy.login({ user: "admin" })
+        cy.get("article.Login")
+
+        cy.login({ password: "admin" })
+        cy.get("article.Login")
     })
 })
